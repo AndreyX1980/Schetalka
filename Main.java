34a0f36e9c7e3a10447262ab16bafac148857aea
AndreyX1980 <77242269+AndreyX1980@users.noreply.github.com>
2023-01-17 22:34:29 +0500

    загрузка

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+package calculator;
+
+import view.CalculatorView;
+
+public class Main {
+
+        public static void main(String[] args) {
+        new CalculatorView().setVisible(true);
+    }
+
+}
